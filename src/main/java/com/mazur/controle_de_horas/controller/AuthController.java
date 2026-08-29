24ad3dae57633fba1e26e2ae59ea2487499fe09c
Gustavo Bueno Mazur <gustavo_bueno_mazur@hotmail.com>
2026-08-29 00:58:00 -0300

    controller

--- src/main/java/com/mazur/controle_de_horas/controller/AuthController.java
+++ src/main/java/com/mazur/controle_de_horas/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
-
+//
     private final AuthService authService;
 
     @PostMapping("/register")
